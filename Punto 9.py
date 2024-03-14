f06e6dd280221f00c99618c23f57d5759761fc7a
JuanDCardenas <159057545+JuanDCardenas@users.noreply.github.com>
2024-03-13 19:32:39 -0500

    Add files via upload

---
+++ Punto 9.py
@@ -0,0 +1,65 @@
+nombre_pais=str(input("Escribe el nombre de un país de Ámerica en minusculas: "))
+if nombre_pais == "argentina":
+    print("Buenos Aires")
+elif nombre_pais == "bahamas":
+    print("Nasáu")
+elif nombre_pais == "barbados":
+    print ("Bridgetown")
+elif nombre_pais == "belice":
+    print ("Belmopán")
+elif nombre_pais == "bolivia":
+    print ("Sucre")
+elif nombre_pais == "brasil":
+    print("Brasilia")
+elif nombre_pais == "canada":
+    print("Ottawa")
+elif nombre_pais == "chile":
+    print("Santiago")
+elif nombre_pais == "colombia":
+    print ("Bogotá")
+elif nombre_pais == "costa rica":
+    print ("San José")
+elif nombre_pais == "cuba":
+    print ("La Habana")
+elif nombre_pais == "dominica":
+    print ("Roseau")
+elif nombre_pais == "ecuador":
+    print ("Quito")
+elif nombre_pais == "el salvador":
+    print ("San Salvador")
+elif nombre_pais == "granada":
+    print ("Saint George")
+elif nombre_pais == "estados unidos":
+    print ("Washington D.C")
+elif nombre_pais == "guatemala":
+    print ("Ciudad de Guatemala")
+elif nombre_pais == "guyana":
+    print ("Georgetown")
+elif nombre_pais == "haiti":
+    print ("Puerto Príncipe")
+elif nombre_pais == "honduras":
+    print ("Tegucigalpa")
+elif nombre_pais == "jamaica":
+    print ("Kingstone")
+elif nombre_pais == "mexico":
+    print ("Ciudad de México")
+elif nombre_pais == "nicaragua":
+    print ("Managua")
+elif nombre_pais == "panama":
+    print ("Ciudad de Panamá")
+elif nombre_pais == "paraguay":
+    print ("Asunción")
+elif nombre_pais == "peru":
+    print ("Lima")
+elif nombre_pais == "colombia":
+    print ("Bogotá")
+elif nombre_pais == "republica dominicana":
+    print ("Santo Domingo")
+elif nombre_pais == "surinam":
+    print ("paramaribo")
+elif nombre_pais == "uruguay":
+    print ("Montevideo")
+elif nombre_pais == "venzuela":
+    print ("Caracas")
+else:
+    print ("Este no es un país de ámerica")
